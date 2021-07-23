a974055bf873614ca848396d471388d2cd034ff2
sunaina-2007 <80744244+sunaina-2007@users.noreply.github.com>
2021-07-23 17:43:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import TransactionScreen  from './screens/BookTransactionScreen';
+import SearchScreen from './screens/SearchScreen';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+export default class App extends React.Component{
+  render(){
+  return <AppContainer/> 
+}}
+
+const TabNavigator = createBottomTabNavigator({
+Transaction: {screen: TransactionScreen} , 
+Search: {screen: SearchScreen}
+})
+
+const AppContainer = createAppContainer(TabNavigator)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
